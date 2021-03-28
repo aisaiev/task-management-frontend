e62b008e3fa749174629e84b2cb49081ed6bfe04
Anton Isaev <aisaev@intetics.com>
2021-03-28 17:22:20 +0300

    task component created

--- src/app/task/task.component.ts
+++ src/app/task/task.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { TaskStatus } from './enum/task-status.enum';
+import { Task } from './models/task.model';
 import { TaskService } from './task.service';
 
 @Component({
@@ -6,9 +13,63 @@ import { TaskService } from './task.service';
   templateUrl: './task.component.html',
   styleUrls: ['./task.component.scss']
 })
-export class TaskComponent implements OnInit {
+export class TaskComponent implements OnInit, OnDestroy {
+  tasks: Task[] = [];
+  taskStatusEnum: typeof TaskStatus = TaskStatus;
+  search = new FormControl('');
+  statusFilter = new FormControl('');
+  subscriptions: Subscription[] = [];
 
-  constructor(private taskService: TaskService) { }
+  constructor(
+    private taskService: TaskService,
+    private authService: AuthService
+  ) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getTasks();
+
+    const searchSubscription = this.search.valueChanges
+      .pipe(
+        debounceTime(200),
+        distinctUntilChanged()
+      )
+      .subscribe(() => this.getTasks());
+    this.subscriptions.push(searchSubscription);
+
+    const statusFilterSubscription =  this.statusFilter.valueChanges.subscribe(() => this.getTasks());
+    this.subscriptions.push(statusFilterSubscription);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => subscription.unsubscribe());
+  }
+
+  getTasks(): void {
+    const filterDto: GetTasksFilterDto = {
+      search: this.search.value,
+      status: this.statusFilter.value
+    };
+    const subscription = this.taskService.getTasks(filterDto).subscribe(tasks => {
+      this.tasks = tasks;
+    });
+    this.subscriptions.push(subscription);
+  }
+
+  taskStatusChange(task: Task): void {
+    const { id, status } = task;
+    const subscription = this.taskService.updateTaskStatus(id, status).subscribe();
+    this.subscriptions.push(subscription);
+  }
+
+  deleteTask(id: number): void {
+    const idx = this.tasks.findIndex(task => task.id === id);
+    const subscription = this.taskService.deleteTask(id).subscribe(() => {
+      this.tasks.splice(idx, 1);
+    });
+    this.subscriptions.push(subscription);
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
 }
--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TaskComponent } from './task.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { TaskComponent } from './task.component';
 import { taskRoute } from './task.route';
+import { plus, trash, boxArrowInRight, NgxBootstrapIconsModule } from 'ngx-bootstrap-icons';
+import { CreateTaskComponent } from './create-task/create-task.component';
+
+const icons = {
+  plus,
+  trash,
+  boxArrowInRight
+};
 
 @NgModule({
-  declarations: [TaskComponent],
+  declarations: [TaskComponent, CreateTaskComponent],
   imports: [
     CommonModule,
-    RouterModule.forChild(taskRoute)
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forChild(taskRoute),
+    NgxBootstrapIconsModule.pick(icons)
   ]
 })
 export class TaskModule { }
