88d58607a955873c773a21ee3a6c3e4af78a7fef
antonio.de.tores@gmail.com <aisaev@intetics.com>
2021-03-29 14:04:42 +0300

    added notifications on fail

--- src/app/task/task.service.ts
+++ src/app/task/task.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { NotificationSeverity } from '../shared/notification/models/notification.model';
+import { NotificationService } from '../shared/notification/notification.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatus } from './enum/task-status.enum';
@@ -14,7 +17,8 @@ export class TaskService {
   private url = environment.apiUrl + '/tasks';
 
   constructor(
-    private httpClient: HttpClient
+    private httpClient: HttpClient,
+    private notificationService: NotificationService
   ) { }
 
   getTasks(filterDto: GetTasksFilterDto): Observable<Task[]> {
@@ -23,19 +27,63 @@ export class TaskService {
       .filter(value => value)
       .join('&');
     const url = query ? `${this.url}?${query}` : this.url;
-    return this.httpClient.get<Task[]>(url);
+    return this.httpClient.get<Task[]>(url)
+      .pipe(
+        catchError(() => {
+          this.notificationService.show({
+            header: 'Tasks Notification',
+            body: 'Unable to retrieve tasks',
+            severity: NotificationSeverity.DANGER,
+            delay: 10000
+          })
+          return EMPTY;
+        })
+      );
   }
 
   createTask(createTaskDto: CreateTaskDto): Observable<Task> {
-    return this.httpClient.post<Task>(this.url, createTaskDto);
+    return this.httpClient.post<Task>(this.url, createTaskDto)
+      .pipe(
+        catchError(() => {
+          this.notificationService.show({
+            header: 'Tasks Notification',
+            body: 'Unable to create task',
+            severity: NotificationSeverity.DANGER,
+            delay: 10000
+          })
+          return EMPTY;
+        })
+      );
   }
 
   updateTaskStatus(id: number, status: TaskStatus): Observable<Task> {
     const body = { status };
-    return this.httpClient.patch<Task>(`${this.url}/${id}/status`, body);
+    return this.httpClient.patch<Task>(`${this.url}/${id}/status`, body)
+      .pipe(
+        catchError(() => {
+          this.notificationService.show({
+            header: 'Tasks Notification',
+            body: 'Unable to update task status',
+            severity: NotificationSeverity.DANGER,
+            delay: 10000
+          })
+          return EMPTY;
+        })
+      );
   }
 
   deleteTask(id: number): Observable<object> {
-    return this.httpClient.delete(`${this.url}/${id}`);
+    return this.httpClient.delete(`${this.url}/${id}`)
+      .pipe(
+        catchError(() => {
+          this.notificationService.show({
+            header: 'Tasks Notification',
+            body: `Unable to delete task ID ${id}`,
+            severity: NotificationSeverity.DANGER,
+            delay: 10000
+          })
+          return EMPTY;
+        })
+      );
   }
 }
